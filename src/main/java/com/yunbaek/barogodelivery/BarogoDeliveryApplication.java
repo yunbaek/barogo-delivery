856c3f7f91ebf7b2d871ccd07c72f824bcd4cd97
yunbaek <yunbaek4564@gmail.com>
2023-02-06 23:01:12 +0900

    init

---
+++ src/main/java/com/yunbaek/barogodelivery/BarogoDeliveryApplication.java
@@ -0,0 +1,13 @@
+package com.yunbaek.barogodelivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BarogoDeliveryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BarogoDeliveryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yunbaek/barogodelivery/BarogoDeliveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yunbaek.barogodelivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BarogoDeliveryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

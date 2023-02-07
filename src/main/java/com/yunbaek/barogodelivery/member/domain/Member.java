07d7b4527e8440e9c17f6b4112c9948ed31b384c
yunbaek <yunbaek4564@gmail.com>
2023-02-07 09:26:55 +0900

    feat(Member): Member domain 추가
    - validation 추가
    - 테스트 코드 작성

--- src/main/java/com/yunbaek/barogodelivery/member/domain/Member.java
+++ src/main/java/com/yunbaek/barogodelivery/member/domain/Member.java
@@ -5,22 +5,50 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+import org.springframework.util.Assert;
+
 @Entity
 public class Member {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long id;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
 
-    private String name;
+	private String name;
 
-    private String password;
+	private String password;
 
-    protected Member() {
-    }
+	protected Member() {
+	}
 
-    public Member(String name, String password) {
-        this.name = name;
-        this.password = password;
-    }
+	public Member(String name, String password) {
+		Assert.hasText(name, "name must not be null");
+		Assert.hasText(password, "password must not be null");
+		Assert.isTrue(isPasswordFormat(password), String.format("%s 비밀번호 형식이 잘못되었습니다.", password));
+		this.name = name;
+		this.password = password;
+	}
+
+	private boolean isPasswordFormat(String password) {
+		String minMaxLength = "^[\\s\\S]{12,}$";
+		String upper = "[A-Z]";
+		String lower = "[a-z]";
+		String number = "\\d";
+		String special = "[$&+,:;=\\\\?@#|/'<>.^*()%!-]";
+		int count = 0;
+
+		if (password.matches(minMaxLength)) {
+			// Only need 3 out of 4 of these to match
+			if (password.matches(".*" + upper + ".*"))
+				count++;
+			if (password.matches(".*" + lower + ".*"))
+				count++;
+			if (password.matches(".*" + number + ".*"))
+				count++;
+			if (password.matches(".*" + special + ".*"))
+				count++;
+		}
+
+		return count >= 3;
+	}
 }
--- src/test/java/com/yunbaek/barogodelivery/member/domain/MemberTest.java
+++ src/test/java/com/yunbaek/barogodelivery/member/domain/MemberTest.java
@@ -1,9 +1,15 @@
 package com.yunbaek.barogodelivery.member.domain;
 
+import static org.assertj.core.api.Assertions.*;
+
+import java.util.stream.Stream;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
 
 @DisplayName("Member Entity 테스트")
 class MemberTest {
@@ -11,8 +17,91 @@ class MemberTest {
     @DisplayName("Member 생성 테스트")
     @Test
     void createMemberTest() {
-        assertDoesNotThrow(() ->
-                new Member("test", "test"));
+        assertThatNoException()
+                .isThrownBy(
+                        () -> new Member("test", "test")
+                );
     }
 
+    @DisplayName("이름이 null 이거나 비어있는 경우 예외 발생 테스트")
+    @ParameterizedTest
+    @NullAndEmptySource
+    void createMemberWithNullNameTest(String name) {
+        assertThatIllegalArgumentException()
+                .isThrownBy(
+                        () -> new Member(name, "test")
+                );
+    }
+
+    @DisplayName("비밀번호가 null 이거나 비어있는 경우 예외 발생 테스트")
+    @ParameterizedTest
+    @NullAndEmptySource
+    void createMemberWithNullPasswordTest(String password) {
+        assertThatIllegalArgumentException()
+                .isThrownBy(
+                        () -> new Member("test", password)
+                );
+    }
+
+    @DisplayName("비밀번호가 12자리 미만이면 예외 발생 테스트")
+    @ParameterizedTest(name = "[{index}] {0} => {1}")
+    @MethodSource
+    void createMemberWithInvalidPasswordLengthTest(String description, String password) {
+        assertThatIllegalArgumentException()
+                .isThrownBy(
+                        () -> new Member("test", password)
+                ).withMessageContaining("비밀번호 형식이");
+    }
+
+       private static Stream<Arguments> createMemberWithInvalidPasswordLengthTest() {
+        return Stream.of(
+            Arguments.of("12자 미만의 비밀번호(문자열 검증조건 충족)", "12abAB!@"),
+            Arguments.of("12자 미만의 비밀번호(문자열 검증조건 미충족)", "12abAB"),
+            Arguments.of("12자 미만의 비밀번호(문자열 검증조건 미충족)", "1234567890")
+        );
+    }
+
+    @DisplayName("비밀번호가 소문자, 대문자, 숫자, 특수문자 중 3개 이상이 포함된 12자리 이상의 숫자가 아니면 예외 발생 테스트")
+    @ParameterizedTest(name = "[{index}] {0} => {1}")
+    @MethodSource
+    void createMemberWithInvalidPasswordTest(String description, String password) {
+        assertThatIllegalArgumentException()
+                .isThrownBy(
+                        () -> new Member("test", password)
+                ).withMessageContaining("");
+    }
+
+    private static Stream<Arguments> createMemberWithInvalidPasswordTest() {
+        return Stream.of(
+            Arguments.of("소문자만으로 이루어진 12자 이상의 비밀번호", "aaaaaaaaaaaa"),
+            Arguments.of("대문자만으로 이루어진 12자 이상의 비밀번호", "AAAAAAAAAAAA"),
+            Arguments.of("숫자만으로 이루어진 12자 이상의 비밀번호", "123456789012"),
+            Arguments.of("특수문자만으로 이루어진 12자 이상의 비밀번호", "!!!!!!!!!!!!"),
+            Arguments.of("소문자와 대문자만으로 이루어진 12자 이상의 비밀번호", "aAaAaAaAaAaA"),
+            Arguments.of("소문자와 숫자만으로 이루어진 12자 이상의 비밀번호", "a1a1a1a1a1a1"),
+            Arguments.of("소문자와 특수문자만으로 이루어진 12자 이상의 비밀번호", "a!a!a!a!a!a!"),
+            Arguments.of("대문자와 숫자만으로 이루어진 12자 이상의 비밀번호", "A1A1A1A1A1A1"),
+            Arguments.of("대문자와 특수문자만으로 이루어진 12자 이상의 비밀번호", "A!A!A!A!A!A!"),
+            Arguments.of("숫자와 특수문자만으로 이루어진 12자 이상의 비밀번호", "1!1!1!1!1!1!")
+        );
+    }
+
+    @DisplayName("비밀번호가 소문자, 대문자, 숫자, 특수문자 중 3개 이상이 포함된 12자리 이상의 숫자이면 예외 발생하지 않음 테스트")
+    @ParameterizedTest(name = "[{index}] {0} => {1}")
+    @MethodSource
+    void createMemberWithValidPasswordTest(String description, String password) {
+        assertThatCode(
+                () -> new Member("test", password)
+        ).doesNotThrowAnyException();
+    }
+
+    private static Stream<Arguments> createMemberWithValidPasswordTest() {
+        return Stream.of(
+            Arguments.of("소문자, 대문자, 숫자, 특수문자 모두 포함", "aA1!aA1!aA1!"),
+            Arguments.of("소문자, 대문자, 숫자 포함", "aA1aA1aA1aA1"),
+            Arguments.of("소문자, 대문자, 특수문자 포함", "aA!aA!aA!aA!"),
+            Arguments.of("소문자, 숫자, 특수문자 포함", "a1!a1!a1!a1!"),
+            Arguments.of("대문자, 숫자, 특수문자 포함", "A1!A1!A1!A1!"
+            ));
+    }
 }

c3b3ae821e93889698298b058a3045640e62d350
yunbaek <yunbaek4564@gmail.com>
2023-02-07 23:07:41 +0900

    feat: 공통 예외 처리 advice 추가

---
+++ src/main/java/com/yunbaek/barogodelivery/common/exception/CommonExceptionHandler.java
@@ -0,0 +1,20 @@
+package com.yunbaek.barogodelivery.common.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class CommonExceptionHandler {
+
+    @ExceptionHandler({DuplicateDataException.class, InvalidDataException.class, NotFoundException.class, IllegalArgumentException.class})
+    public ResponseEntity<String> handleDuplicateAndInvalidDataException(RuntimeException e) {
+        return ResponseEntity.badRequest().body(e.getMessage());
+    }
+
+    @ExceptionHandler(AuthorizationException.class)
+    public ResponseEntity<String> handleAuthorizationException() {
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+    }
+}

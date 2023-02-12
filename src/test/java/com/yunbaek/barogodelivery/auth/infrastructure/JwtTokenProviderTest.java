61dc7aadad4d4d6468dc2a9950104169610e4f2f
yunbaek <yunbaek4564@gmail.com>
2023-02-12 14:14:47 +0900

    fix: 토큰 생성 테스트 수정

--- src/test/java/com/yunbaek/barogodelivery/auth/infrastructure/JwtTokenProviderTest.java
+++ src/test/java/com/yunbaek/barogodelivery/auth/infrastructure/JwtTokenProviderTest.java
@@ -6,14 +6,12 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.test.util.ReflectionTestUtils;
 
-import java.util.ArrayList;
-
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.BDDMockito.given;
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
+import static org.mockito.Mockito.mock;
 
 @DisplayName("JWT 토큰 생성 테스트")
 @ExtendWith(MockitoExtension.class)
@@ -29,15 +27,14 @@ class JwtTokenProviderTest {
     @Test
     void createTokenTest() {
         // given
-        User user = new User("test", "test", new ArrayList<>());
-        given(userDetailsService.loadUserByUsername(any())).willReturn(user);
+        Authentication mock = mock(Authentication.class);
         ReflectionTestUtils.setField(jwtTokenProvider, "secretKey", "test");
         ReflectionTestUtils.setField(jwtTokenProvider, "validityInMilliseconds", 1000L);
 
         // when
-//        String token = jwtTokenProvider.createToken(authentication);
+        String token = jwtTokenProvider.createToken(mock);
 
         // then
-//        assertThat(token).isNotNull();
+        assertThat(token).isNotNull();
     }
 }

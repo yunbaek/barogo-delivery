ac33462aa9701d39264f6e289efa988048cbdba2
yunbaek <yunbaek4564@gmail.com>
2023-02-12 14:07:14 +0900

    feat: DeliveryRepository 추가

---
+++ src/main/java/com/yunbaek/barogodelivery/delivery/domain/DeliveryRepository.java
@@ -0,0 +1,6 @@
+package com.yunbaek.barogodelivery.delivery.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DeliveryRepository extends JpaRepository<Delivery, Long> {
+}
